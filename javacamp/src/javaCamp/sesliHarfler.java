5f69cc4282b73eb8039bf15713653fde762d09b5
ARPAT23 <serghei.duloglu@bil.omu.edu.tr>
2022-09-21 17:06:49 +0300

    Java Kampı ilk hafta ödevleri
    Java kapmında verilin ödev videolarının kodlanıp anlaşılması.

---
+++ javacamp/src/javaCamp/sesliHarfler.java
@@ -0,0 +1,24 @@
+package javaCamp;
+
+public class sesliHarfler {
+	
+	 public static void main(String[] args) {
+
+	        char harf='E';
+
+	        switch (harf){
+	            case 'A':
+	            case 'I':
+	            case 'O':
+	            case 'U':
+
+	                System.out.println("kalÄ±n sesli harf");
+	                break;
+	            default:
+	                System.out.println("ince sesli harfler");
+
+	        }
+
+	    }
+	}
+
